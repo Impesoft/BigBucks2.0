db40a0434c5ac573387f2dd0a4907a2b06e78e8a
MrBlubke <ward@impesoft.com>
2021-02-03 13:10:33 +0100

    commit

--- BigBucks2.0/Interfaces/IBeverage.cs
+++ BigBucks2.0/Interfaces/IBeverage.cs
@@ -3,16 +3,16 @@ using System.Windows.Controls;
 
 namespace BigBucks2._0
 {
-    interface IBeverage
+    internal interface IBeverage
     {
-        string DefaultImage { get; set; }
         string Description { get; set; }
         string Image { get; set; }
-        int InstanceID { get; set; }
+        int Instanceid { get; set; }
         string Name { get; set; }
         double Price { get; set; }
 
         List<Control> CreateControls();
+
         string ToString();
     }
 }
