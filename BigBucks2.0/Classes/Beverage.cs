9cc9e799d5746058468bba38b7616a7e31c082d0
MrBlubke <ward@impesoft.com>
2021-02-03 12:25:24 +0100

    Add project files.

---
+++ BigBucks2.0/Classes/Beverage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace BigBucks2._0
+{
+    internal abstract class Beverage : IBeverage
+    {
+        private double _price;
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Description { get; set; }
+
+        private static int instanceIDCounter = 1;
+
+        public string DefaultImage { get; set; }
+
+        public int InstanceID { get; set; }
+
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+
+        protected Beverage()
+        {
+            InstanceID = instanceIDCounter;
+            instanceIDCounter++;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Price}, {Description}, {InstanceID}, {Image}, {Name}";
+            return result;
+        }
+
+        public virtual List<Control> CreateControls()
+        {
+            return null;
+        }
+    }
+}
---
+++ BigBucks2.0/Interfaces/IBeverage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace BigBucks2._0
+{
+    interface IBeverage
+    {
+        string DefaultImage { get; set; }
+        string Description { get; set; }
+        string Image { get; set; }
+        int InstanceID { get; set; }
+        string Name { get; set; }
+        double Price { get; set; }
+
+        List<Control> CreateControls();
+        string ToString();
+    }
+}
